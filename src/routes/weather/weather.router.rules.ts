d83c6a3e01adb892bced2d2a7f85a54444b274de
Borisa Krstic <borisakrstic02@gmail.com>
2021-11-11 19:34:25 +0100

    Cleaned up the weather router

--- src/routes/weather/weather.router.rules.ts
+++ src/routes/weather/weather.router.rules.ts
@@ -1,7 +1,5 @@
 import { body } from 'express-validator'
 
 export default {
-  get: [],
-  get_id: [body('city').not().isEmpty()],
-  post_id_test: [body('city').not().isEmpty()],
+  get: [body('city').not().isEmpty()],
 }
--- src/routes/weather/weather.router.ts
+++ src/routes/weather/weather.router.ts
@@ -3,9 +3,5 @@ import { router } from '../../server_setup'
 import { weatherController } from '../../controllers'
 
 router.get('/', weatherController.get)
-router.get('/:id', weatherController.get)
-router.get('/all/test', weatherController.get)
-
-router.post('/:id/test', weatherController.get)
 
 export default router.getInstance()
