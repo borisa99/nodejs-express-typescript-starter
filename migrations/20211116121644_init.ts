8f3310087c6347bfbb11e3c829a61854518abc46
Borisa Krstic <borisakrstic02@gmail.com>
2021-11-16 13:52:52 +0100

    Added knex init migration and seed

--- migrations/20211116121644_init.ts
+++ migrations/20211116121644_init.ts
@@ -1,7 +1,10 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  //Add PG CRYPTO extension
   await knex.raw('CREATE EXTENSION IF NOT EXISTS pgcrypto')
+
+  //CREATE users TABLE
   await knex.schema.createTable('users', table => {
     table.uuid('id').primary().defaultTo(knex.raw('public.gen_random_uuid()'))
     table.string('first_name').notNullable()
@@ -10,8 +13,63 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
   })
-}
 
+  //CREATE roles TABLE
+  await knex.schema.createTable('roles', table => {
+    table.string('role').primary().notNullable()
+    table.string('comment').notNullable()
+  })
+
+  //CREATE accounts TABLE
+  await knex.schema.createTable('accounts', table => {
+    table.uuid('id').primary().defaultTo(knex.raw('public.gen_random_uuid()'))
+    table.string('email').notNullable()
+    table.string('password_hash').notNullable()
+    table.uuid('ticket').notNullable()
+    table.timestamp('ticket_expires_at').notNullable()
+    table.boolean('is_active').notNullable().defaultTo(false)
+    table.uuid('user_id').notNullable().references('id').inTable('users')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.index('user_id')
+  })
+
+  //CREATE account_roles TABLE
+  await knex.schema.createTable('account_roles', table => {
+    table.uuid('id').primary().defaultTo(knex.raw('public.gen_random_uuid()'))
+    table.uuid('account_id').notNullable().references('id').inTable('accounts')
+    table.string('role').notNullable().references('role').inTable('roles')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.index('account_id')
+    table.index('role')
+  })
+
+  //CREATE refresh_tokens TABLE
+  await knex.schema.createTable('refresh_tokens', table => {
+    table
+      .uuid('refresh_token')
+      .primary()
+      .defaultTo(knex.raw('public.gen_random_uuid()'))
+    table.timestamp('expires_at').notNullable()
+    table.uuid('account_id').notNullable().references('id').inTable('accounts')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.index('account_id')
+  })
+}
 export async function down(knex: Knex): Promise<void> {
+  //DROP refresh_tokens TABLE
+  await knex.schema.dropTable('refresh_tokens')
+
+  // DROP accounts TABLE
+  await knex.schema.dropTable('account_roles')
+
+  // DROP accounts TABLE
+  await knex.schema.dropTable('accounts')
+
+  // DROP roles TABLE
+  await knex.schema.dropTable('roles')
+
+  // DROP users TABLE
   await knex.schema.dropTable('users')
 }
