0767b967fde546269f668b522ce9adbf3d10fafd
Borisa Krstic <borisakrstic02@gmail.com>
2021-11-17 12:45:43 +0100

    Added inserting acount when registering user

--- src/services/auth/AuthService.ts
+++ src/services/auth/AuthService.ts
@@ -1,10 +1,13 @@
 import { Service } from 'typedi'
+import { v4 as uuidv4 } from 'uuid'
+import dayjs from 'dayjs'
 import { IAuthService } from './IAuthService'
 import { RegisterUser } from '@/shared/types/auth/RegisterUser'
 import db from '@/shared/db'
 
 import { ServiceResponse } from '@/models/ServiceResponse'
 import { Account } from '@/models/Account'
+import { User } from '@/models/User'
 
 import { hashPassword } from '@shared/bcrypt'
 
@@ -22,11 +25,32 @@ export class AuthService implements IAuthService {
         response.error = 'Email already exists'
         return response
       }
-      const hashedPassword = await hashPassword(user.password)
+
+      //Create new user
+      const [user_id]: string = await db<User>('users').returning('id').insert({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        avatar_url: user.avatar_url,
+      })
+
+      //Create new account
+      const ticket = uuidv4()
+      const [account_id]: string = await db<Account>('accounts')
+        .returning('id')
+        .insert({
+          user_id,
+          email: user.email,
+          password_hash: await hashPassword(user.password),
+          ticket,
+          ticket_expires_at: dayjs().add(1, 'day').toDate(),
+        })
       console.log(
-        '🚀 ~ file: AuthService.ts ~ line 26 ~ AuthService ~ register ~ hashedPassword',
-        hashedPassword
+        '🚀 ~ file: AuthService.ts ~ line 39 ~ AuthService ~ register ~ account_id',
+        account_id
       )
+
+      // Insert account roles
+
       response.payload = 'Success'
     } catch (error: any) {
       response.status = 500
