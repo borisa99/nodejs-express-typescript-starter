346af2cab6c6f30d6944d5fd43e80f290a31af10
Radan Stupar <radan.s@hotmail.com>
2021-11-17 11:47:22 +0100

    DB driver moved to .env, added tests, readme file
    Added jest as testing framework so all things that are created can be tested
    Created one test for for database model based queries, updated readme with examples, added lodash

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,14 @@
-export interface User {
-  id: string
-  first_name: string
-  last_name: string
-  avatar_url: string
-  created_at: Date
-  updated_at: Date
+import Model from '../shared/Model'
+
+export class User extends Model {
+  avatar_url: string | undefined
+  created_at: Date | undefined
+  first_name: string | undefined
+  id: string | undefined
+  last_name: string | undefined
+  updated_at: Date | undefined
+
+  constructor() {
+    super()
+  }
 }
