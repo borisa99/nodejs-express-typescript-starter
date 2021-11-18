69186ee02d5c0c11f855230fc63fa4f5b432b61d
Borisa Krstic <borisakrstic02@gmail.com>
2021-11-18 08:45:29 +0100

    Implemented generateRefreshToken method

--- src/shared/jwt.ts
+++ src/shared/jwt.ts
@@ -1,8 +1,31 @@
 import jwt from 'jsonwebtoken'
-import { TokenPayload } from './types/auth/TokenPayload'
+import { v4 as uuidv4 } from 'uuid'
 
-export const generateToken = async (tokenPayload: TokenPayload) => {
+import { TokenPayload } from './types/auth/TokenPayload'
+import db from './db'
+
+import { RefreshToken } from '@/models/RefreshToken'
+
+export const generateToken = async (
+  tokenPayload: TokenPayload
+): Promise<string> => {
   return await jwt.sign(tokenPayload, <string>process.env.JWT_SECRET, {
     expiresIn: '1d',
   })
+}
+
+export const generateRefreshToken = async (
+  account_id: string
+): Promise<string> => {
+  // Delete old refresh tokens
+  await db<RefreshToken>('refresh_tokens').where({ account_id }).del()
+
+  // Generate new refresh token
+  const [refreshToken]: string = await db<RefreshToken>('refresh_tokens')
+    .returning('refresh_token')
+    .insert({
+      refresh_token: uuidv4(),
+      account_id,
+    })
+  return refreshToken
 }
