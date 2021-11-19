974045d6a62c70ef8d8ba92fd9e1b10a1162dba8
Borisa Krstic <borisakrstic02@gmail.com>
2021-11-19 09:06:38 +0100

    Created auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { ServiceResponse } from '@/models/ServiceResponse'
+import { serviceResponseHandler } from '@/shared/serviceResponseHandler'
+import jwt from 'jsonwebtoken'
+
+export const auth = (req: any, res: any, next: any) => {
+  const result: ServiceResponse<any> = new ServiceResponse<any>()
+  try {
+    const token = req.header('token')
+
+    // Check if token exists
+    if (!token) {
+      result.error = 'No token, authorization denied'
+      result.status = 401
+    }
+    // Check if token is valid
+    const decoded = jwt.verify(token, <string>process.env.JWT_SECRET)
+
+    // TODO Check if token is expired
+
+    // Set user to req
+    req.user = decoded
+  } catch (error: any) {
+    result.error = error.message
+    result.status = 401
+  } finally {
+    result.status === 200 ? next() : serviceResponseHandler(res, result)
+  }
+}
